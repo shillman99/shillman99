7b5d27da3e8846382d6bddb3ffaf1fd372ba2466
shillman99 <sam.hillman99@gmail.com>
2024-08-26 21:57:21 -0500

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,19 @@
+# Literal
+skidaddle = 81
+print("Literal:", skidaddle)
+
+# Integer
+scram = -96
+print("Integer:", scram)
+
+# Float
+go = 15.5
+print("Float:", go)
+
+# String
+lions = "John"
+print("String:", lions)
+
+# Boolean
+win = True
+print("Boolean:", win)
